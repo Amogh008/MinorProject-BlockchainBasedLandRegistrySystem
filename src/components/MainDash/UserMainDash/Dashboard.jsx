62ecad93bfd032c350822f656396089d7ad49e21
Amogh008 <70563182+Amogh008@users.noreply.github.com>
2022-06-08 19:23:11 +0530

    Rectified abi error

--- src/components/MainDash/UserMainDash/Dashboard.jsx
+++ src/components/MainDash/UserMainDash/Dashboard.jsx
@@ -1,5 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Land } from "./../../../Contract/LandContract";
+import { web3 } from "./../../../Contract/LandContract";
 const Dashboard = () => {
+  useEffect(() => {
+    const checkManager = async () => {
+      const accounts = await web3.eth.getAccounts();
+      // const add = ["0x71B90A60b4120ab43E0E6b857a1736925d7e831B"];
+      const msg = await Land.methods.isContractOwner(accounts[0]).call();
+      console.log(msg);
+    };
+    checkManager();
+  }, []);
   return (
     <div className="co">
       This page which holds the transaction to the contract is under
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
@@ -7,11 +6,9 @@ const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
-  <StrictMode>
-    <ContextProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ContextProvider>
-  </StrictMode>
+  <ContextProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ContextProvider>
 );
